21edaa351c3e46a68cc26cf6234b4fbf8ebb433f
Swarnika Shakya <sswarnika@tbc.edu.np>
2021-10-31 08:21:42 +0545

    Models added

---
+++ app/Models/Calls.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Calls extends Model
+{
+    use HasFactory;
+}
