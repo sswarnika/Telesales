697bbd90bd57b3d8203c4d7c0d8d5c994fb766ea
Swarnika Shakya <sswarnika@tbc.edu.np>
2022-01-26 17:50:59 +0545

    Reset Password Error Solved

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
                             <div class="form-group row" style="margin-right: 45px;">
                                 <div class="col-md-6 offset-md-4">
                                     <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
                                             {{ __('Remember Me') }}
